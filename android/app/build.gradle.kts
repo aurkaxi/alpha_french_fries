2c14b8c30feb38470c48b7b17cb2cbcef68f4865
aurkaxi <abdullahalmuaz15@gmail.com>
2025-03-23 01:14:02 +0600

    Add new splash and branding images, update asset paths in configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.alpha_french_fries"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
