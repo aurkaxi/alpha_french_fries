aef5ce5e90309b3655489c019a9a3c2b7af45007
aurkaxi <abdullahalmuaz15@gmail.com>
2025-04-27 20:07:35 +0600

    get opcodes from a map

--- lib/operation.dart
+++ lib/operation.dart
@@ -8,5 +8,18 @@ enum Operation {
   rightShift,
   increment,
   decrement,
-  xor
+  xor,
 }
+
+Map<Operation, int> operationMap = {
+  Operation.addition: 0,
+  Operation.increment: 0,
+  Operation.and: 1,
+  Operation.or: 2,
+  Operation.xor: 3,
+  Operation.negation: 4,
+  Operation.leftShift: 5,
+  Operation.rightShift: 6,
+  Operation.subtraction: 8,
+  Operation.decrement: 8,
+};
